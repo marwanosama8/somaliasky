80792a85b65a947455cdc90f0a3f1f7980bf80ec
Marwan Osama <90649175+marwanosama8888@users.noreply.github.com>
2022-12-19 18:08:01 +0200

    Update 2022_07_21_044624_countries_table_add_currency_id.php

--- database/migrations/2022_07_21_044624_countries_table_add_currency_id.php
+++ database/migrations/2022_07_21_044624_countries_table_add_currency_id.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::table('countries', function (Blueprint $table) {
             $table->unsignedBigInteger('currency_id')->default(3)->after('flag');
 
-            $table->foreign('currency_id')->references('id')->on("currencies")->onDelete('cascade')->onUpdate('cascade');
+//             $table->foreign('currency_id')->references('id')->on("currencies")->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
