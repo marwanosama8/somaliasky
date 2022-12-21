23984a43c02bea2be4ed8fa976befa8970329df1
Marwan Osama <90649175+marwanosama8888@users.noreply.github.com>
2022-12-21 02:32:19 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,7 +35,7 @@ class AppServiceProvider extends ServiceProvider
         // \App\Models\ContactReply::observe(\App\Observers\ContactReplyObserver::class);
         // \App\Models\Contact::observe(\App\Observers\ContactObserver::class);
         // request()->ip();
-\Illuminate\Support\Facades\URL::forceScheme('http');
+\Illuminate\Support\Facades\URL::forceScheme('https');
 
         Paginator::useBootstrapFive();
         Schema::defaultStringLength(191);
