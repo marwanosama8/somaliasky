65d80ed4410b60319d392321b1c3c0482c2c402f
Marwan Osama <90649175+marwanosama8888@users.noreply.github.com>
2022-12-21 02:07:19 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-            $this->app['request']->server->set('HTTP', true);
+//             $this->app['request']->server->set('HTTP', true);
 
     }
 
